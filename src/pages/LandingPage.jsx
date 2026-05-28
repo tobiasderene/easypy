c003c573e8c5c5f6a53c3214563f8178ba3ecdda
tobiasderene <133263634+tobiasderene@users.noreply.github.com>
2026-05-28 16:50:40 -0300

    Update LandingPage.jsx

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from "react";
-import "./LandingPage.css";
+import "./landingpage.css";
 
 const useVisible = (threshold = 0.15) => {
   const ref = useRef(null);
