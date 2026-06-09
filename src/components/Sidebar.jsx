a91c64e7f3804b9cf9eb97d753f00b431a633235
tobiasderene <133263634+tobiasderene@users.noreply.github.com>
2026-06-09 10:35:40 -0300

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -68,7 +68,7 @@ const Sidebar = ({ isOpen, onClose }) => {
             <li className="sidebar-item">
               <button className={`sidebar-link ${activePage === 'devoluciones' ? 'active' : ''}`} onClick={() => handleLinkClick('/devoluciones')}>
                 <svg fill="none" stroke="currentColor" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M16 15v-1a4 4 0 00-4-4H8m0 0l3 3m-3-3l3-3m9 14V5a2 2 0 00-2-2H6a2 2 0 00-2 2v16l4-2 4 2 4-2 4 2z" /></svg>
-                <span>Mis Reclamos</span>
+                <span>Mis Problemas</span>
               </button>
             </li>
 
